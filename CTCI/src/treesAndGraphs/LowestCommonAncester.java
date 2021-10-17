5d35b40addfd9c6ec6d056dca62d569141783207
HamadKhushik <hammadkhan@live.com>
2021-10-17 17:06:57 +0100

    lowest common ancester implementation without using parent links

--- CTCI/src/treesAndGraphs/LowestCommonAncester.java
+++ CTCI/src/treesAndGraphs/LowestCommonAncester.java
@@ -49,8 +49,6 @@ public class LowestCommonAncester {
 			return null;
 		}
 
-		BinaryTreeNode<Integer> result = left != null ? left : right;
-
 		// if one of the nodes found - return that!!
 		return left != null ? left : right;
 	}
@@ -121,4 +119,74 @@ public class LowestCommonAncester {
 		return node;
 	}
 
+	/*
+	 * lowerst common ancester this method searches the second node in the sibling
+	 * subtree once other node is found in the subtree, parent node is the common
+	 * ancester
+	 */
+	public BinaryTreeNode<Integer> commonAncesterSibling(BinaryTreeNode<Integer> root, BinaryTreeNode<Integer> n1,
+			BinaryTreeNode<Integer> n2) {
+
+		if (!covers(root, n1) || !covers(root, n2)) {
+			return null;
+		}
+
+		if (covers(n1, n2)) {
+			return n1;
+		}
+
+		if (covers(n2, n1)) {
+			return n2;
+		}
+
+		BinaryTreeNode<Integer> sibling = getSibling(n1);
+		BinaryTreeNode<Integer> parent = n1.getParent();
+
+		while (!covers(sibling, n2)) {
+			sibling = getSibling(parent);
+			parent = parent.getParent();
+		}
+
+		return parent;
+	}
+
+	public BinaryTreeNode<Integer> getSibling(BinaryTreeNode<Integer> node) {
+
+		if (node == null || node.getParent() == null) {
+			return null;
+		}
+
+		BinaryTreeNode<Integer> parent = node.getParent();
+
+		return parent.getLeft().equals(node) ? parent.getRight() : parent.getLeft();
+	}
+
+	/*
+	 * common ancester soluion 3 without using the parent links this method checks
+	 * if both the nodes are on the same side on the root once we get to a root
+	 * where the other nodes are on the different subtrees, thats out common
+	 * ancester
+	 */
+	public BinaryTreeNode<Integer> commonAncester3(BinaryTreeNode<Integer> root, BinaryTreeNode<Integer> n1,
+			BinaryTreeNode<Integer> n2) {
+
+		if (!covers(root, n1) || !covers(root, n2)) {
+			return null;
+		}
+
+		if (root == null || root.equals(n1) || root.equals(n2)) {
+			return root;
+		}
+
+		boolean n1IsOnLeft = covers(root.getLeft(), n1);
+		boolean n2IsOnLeft = covers(root.getLeft(), n2);
+
+		// if n1 and n2 are on the different sides of the root, 'root' is common
+		// ancester
+		if (n1IsOnLeft != n2IsOnLeft) {
+			return root;
+		}
+
+		return n1IsOnLeft ? root.getLeft() : root.getRight();
+	}
 }
--- CTCI/src/treesAndGraphs/LowestCommonAncesterTest.java
+++ CTCI/src/treesAndGraphs/LowestCommonAncesterTest.java
@@ -76,4 +76,66 @@ public class LowestCommonAncesterTest {
 		assertNull(ancester);
 	}
 
+	@Test
+	public void getSiblingTest() {
+		root = bt.getRoot();
+		bt.printTree(bt.getRoot());
+		BinaryTreeNode<Integer> sibling = lca.getSibling(bt.findNode(root, 6));
+		assertEquals(new BinaryTreeNode<Integer>(8), sibling);
+
+		sibling = lca.getSibling(bt.findNode(root, 2));
+		assertEquals(new BinaryTreeNode<Integer>(11), sibling);
+
+		sibling = lca.getSibling(bt.findNode(root, 5));
+		assertEquals(new BinaryTreeNode<Integer>(9), sibling);
+
+		sibling = lca.getSibling(bt.findNode(root, 7));
+		assertEquals(null, sibling);
+	}
+
+	@Test
+	public void commonAncesterSiblingTest() {
+		root = bt.getRoot();
+		bt.printTree(bt.getRoot());
+		BinaryTreeNode<Integer> ancester = lca.commonAncesterSibling(root, bt.findNode(root, 6), bt.findNode(root, 8));
+		assertEquals(new BinaryTreeNode<Integer>(3), ancester);
+
+		ancester = lca.commonAncesterSibling(root, bt.findNode(root, 11), bt.findNode(root, 2));
+		assertEquals(new BinaryTreeNode<Integer>(6), ancester);
+
+		ancester = lca.commonAncesterSibling(root, bt.findNode(root, 2), bt.findNode(root, 5));
+		assertEquals(new BinaryTreeNode<Integer>(6), ancester);
+
+		ancester = lca.commonAncesterSibling(root, bt.findNode(root, 8), bt.findNode(root, 7));
+		assertEquals(new BinaryTreeNode<Integer>(8), ancester);
+
+		ancester = lca.commonAncesterSibling(root, bt.findNode(root, 8), bt.findNode(root, 20));
+		assertNull(ancester);
+
+		ancester = lca.commonAncesterSibling(root, bt.findNode(root, 20), bt.findNode(root, 13));
+		assertNull(ancester);
+	}
+
+	@Test
+	public void commonAncester3Test() {
+		root = bt.getRoot();
+		bt.printTree(bt.getRoot());
+		BinaryTreeNode<Integer> ancester = lca.commonAncester3(root, bt.findNode(root, 6), bt.findNode(root, 8));
+		assertEquals(new BinaryTreeNode<Integer>(3), ancester);
+
+		ancester = lca.commonAncester3(root, bt.findNode(root, 11), bt.findNode(root, 2));
+		assertEquals(new BinaryTreeNode<Integer>(6), ancester);
+
+		ancester = lca.commonAncester3(root, bt.findNode(root, 2), bt.findNode(root, 5));
+		assertEquals(new BinaryTreeNode<Integer>(6), ancester);
+
+		ancester = lca.commonAncester3(root, bt.findNode(root, 8), bt.findNode(root, 7));
+		assertEquals(new BinaryTreeNode<Integer>(8), ancester);
+
+		ancester = lca.commonAncester3(root, bt.findNode(root, 8), bt.findNode(root, 20));
+		assertNull(ancester);
+
+		ancester = lca.commonAncester3(root, bt.findNode(root, 20), bt.findNode(root, 13));
+		assertNull(ancester);
+	}
 }
