822b1f73a333d3e2e066f4379cab98b73fff3648
HamadKhushik <hammadkhan@live.com>
2021-10-22 01:41:23 +0100

    BST Sequences implementation completed

--- CTCI/src/treesAndGraphs/BSTSequences.java
+++ CTCI/src/treesAndGraphs/BSTSequences.java
@@ -1,5 +1,78 @@
 package treesAndGraphs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * 4.9 BST Sequences
+ * This method returns all the sequence of lists which results in the same binary search tree
+ * This is the book solution and uses two recursive algorithms
+ * REF : https://medium.com/@jackwootton/binary-search-tree-sequences-53163b1f374a
+ */
 public class BSTSequences {
+
+	public List<LinkedList<Integer>> findSequences(BinaryTreeNode<Integer> node) {
+
+		List<LinkedList<Integer>> results = new ArrayList<LinkedList<Integer>>();
+
+		// Base case
+		if (node == null) {
+			results.add(new LinkedList<Integer>());
+			return results;
+		}
+
+		LinkedList<Integer> prefix = new LinkedList<Integer>();
+		prefix.add(node.getData());
+
+		// recurse
+		List<LinkedList<Integer>> leftTree = findSequences(node.getLeft());
+		List<LinkedList<Integer>> rightTree = findSequences(node.getRight());
+
+		for (LinkedList<Integer> first : leftTree) {
+			for (LinkedList<Integer> second : rightTree) {
+				List<LinkedList<Integer>> weaved = new ArrayList<LinkedList<Integer>>();
+				weaveLists(first, second, weaved, prefix);
+				results.addAll(weaved);
+			}
+		}
+		return results;
+	}
+
+	// second recursive function to combine the lists
+	private void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second, List<LinkedList<Integer>> results,
+			LinkedList<Integer> prefix) {
+
+		// base case
+		if (first.size() == 0 || second.size() == 0) {
+			LinkedList<Integer> result = (LinkedList<Integer>) prefix.clone();
+			result.addAll(first);
+			result.addAll(second);
+			results.add(result);
+			return;
+		}
+
+		// recurse with head of first list
+		Integer firstHead = first.removeFirst();
+		prefix.add(firstHead);
+		weaveLists(first, second, results, prefix);
+		firstHead = prefix.removeLast();
+		first.addFirst(firstHead);
+
+		// recurse with head of second list
+		Integer secondHead = second.removeFirst();
+		prefix.add(secondHead);
+		weaveLists(first, second, results, prefix);
+		secondHead = prefix.removeLast();
+		second.addFirst(secondHead);
+	}
+
+	// helper method to print results
+	public void printLists(List<LinkedList<Integer>> results) {
+
+		for (LinkedList<Integer> list : results) {
+			System.out.println(list);
+		}
+	}
 
 }
--- CTCI/src/treesAndGraphs/BSTSequencesTest.java
+++ CTCI/src/treesAndGraphs/BSTSequencesTest.java
@@ -1,19 +1,63 @@
 package treesAndGraphs;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class BSTSequencesTest {
 
+	BinaryTree<Integer> tree = new BinaryTree<Integer>();
+	BinaryTreeNode<Integer> root;
+
+	BSTSequences sequences = new BSTSequences();
+	List<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
+
 	@Before
 	public void setUp() throws Exception {
+
+		tree.add(10);
+		tree.add(5);
+		tree.add(20);
+		tree.add(2);
+		tree.add(30);
+		root = tree.getRoot();
+		tree.printTree(root);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void findSequencesTest() {
+		result = sequences.findSequences(root);
+//		sequences.printLists(result);
+		assertEquals(6, result.size());
+
+		tree = new BinaryTree<Integer>();
+		tree.add(10);
+		tree.add(5);
+		tree.add(20);
+		result = sequences.findSequences(tree.getRoot());
+//		sequences.printLists(result);
+		assertEquals(2, result.size());
+
+		tree = new BinaryTree<Integer>();
+		tree.add(10);
+		tree.add(5);
+		tree.add(20);
+		tree.add(2);
+		tree.add(4);
+		tree.add(30);
+		result = sequences.findSequences(tree.getRoot());
+//		sequences.printLists(result);
+		assertEquals(10, result.size());
+
+		// null node check
+		tree = new BinaryTree<Integer>();
+		result = sequences.findSequences(tree.getRoot());
+		assertEquals(1, result.size());
 	}
 
 }
