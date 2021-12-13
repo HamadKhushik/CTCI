4dd71f49ed2aed2898ee46b49ebefaf17f2c9156
HamadKhushik <hammadkhan@live.com>
2021-12-13 01:38:18 +0000

    8.7 Permutations of a String without duplicates implemented

--- CTCI/src/recursionAndDynamicProgramming/PermutationsWithoutDups.java
+++ CTCI/src/recursionAndDynamicProgramming/PermutationsWithoutDups.java
@@ -6,7 +6,8 @@ package recursionAndDynamicProgramming;
 import java.util.ArrayList;
 
 /**
- * 8.7 Permutations of a String without duplicates
+ * 8.7 Permutations of a String without duplicates. Assume all characters in the
+ * String will be unique
  * 
  * @author Hammad: 12-Dec-2021
  */
@@ -49,4 +50,59 @@ public class PermutationsWithoutDups {
 		return start + first + end;
 	}
 
+	public ArrayList<String> getPerms2(String remainder) {
+
+		if (remainder == null) {
+			return null;
+		}
+
+		int len = remainder.length();
+		ArrayList<String> result = new ArrayList<String>();
+
+		if (len == 0) {
+			result.add("");
+			return result;
+		}
+
+		/*
+		 * get all permutations of n-1 characters and then add nth character in second
+		 * loop
+		 */
+		for (int i = 0; i < len; i++) {
+			String before = remainder.substring(0, i);
+			String after = remainder.substring(i + 1, len);
+			ArrayList<String> partials = getPerms2(before + after);
+
+			for (String s : partials) {
+				result.add(remainder.charAt(i) + s);
+			}
+		}
+		return result;
+	}
+
+	public ArrayList<String> getPerms3(String remainder) {
+
+		if (remainder == null) {
+			return null;
+		}
+
+		ArrayList<String> result = new ArrayList<String>();
+		getPerms3("", remainder, result);
+		return result;
+	}
+
+	private void getPerms3(String prefix, String remainder, ArrayList<String> result) {
+
+		int len = remainder.length();
+		if (len == 0) {
+			result.add(prefix);
+		}
+
+		for (int i = 0; i < len; i++) {
+			String before = remainder.substring(0, i);
+			String after = remainder.substring(i + 1, len);
+			char c = remainder.charAt(i);
+			getPerms3(prefix + c, before + after, result);
+		}
+	}
 }
--- CTCI/src/recursionAndDynamicProgramming/PermutationsWithoutDupsTest.java
+++ CTCI/src/recursionAndDynamicProgramming/PermutationsWithoutDupsTest.java
@@ -41,11 +41,50 @@ public class PermutationsWithoutDupsTest {
 	@Test
 	public void getPermsTest() {
 		ArrayList<String> permutations = testObject.getPerms(str);
-		System.out.println(permutations);
+		System.out.println(permutations + " - First Approach");
 		assertEquals(checkerList, permutations);
 		emptyList.add("");
 		assertEquals(emptyList, testObject.getPerms(empty));
 		assertNull(testObject.getPerms(null));
 	}
 
+	@Test
+	public void getPerms2Test() {
+		checkerList = new ArrayList<String>() {
+			{
+				add("abc");
+				add("acb");
+				add("bac");
+				add("bca");
+				add("cab");
+				add("cba");
+			}
+		};
+		ArrayList<String> permutations = testObject.getPerms2(str);
+		System.out.println(permutations + " - Second Approach");
+		assertEquals(checkerList, permutations);
+		emptyList.add("");
+		assertEquals(emptyList, testObject.getPerms2(empty));
+		assertNull(testObject.getPerms2(null));
+	}
+
+	@Test
+	public void getPerms3Test() {
+		checkerList = new ArrayList<String>() {
+			{
+				add("abc");
+				add("acb");
+				add("bac");
+				add("bca");
+				add("cab");
+				add("cba");
+			}
+		};
+		ArrayList<String> permutations = testObject.getPerms3(str);
+		System.out.println(permutations + " - Third Approach");
+		assertEquals(checkerList, permutations);
+		emptyList.add("");
+		assertEquals(emptyList, testObject.getPerms3(empty));
+		assertNull(testObject.getPerms3(null));
+	}
 }
