8d5ebadeb06bc4eb9d3bd03456a3fed72cc608cc
HamadKhushik <hammadkhan@live.com>
2022-01-07 23:35:12 +0000

    working on the testing chapter in the book

---
+++ CTCI/src/testing/UnsignedTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package testing;
+
+/**
+ * 11.1, find the error in the following code
+ * 
+ * @author Hammad: 07-Jan-2022
+ */
+public class UnsignedTest {
+
+	public static void check() {
+		Unsigned int i;
+	}
+
+}
