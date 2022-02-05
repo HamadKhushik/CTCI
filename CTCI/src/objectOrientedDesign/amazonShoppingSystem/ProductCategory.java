83eaf048cf28185f17d0d9c5cb9f75b658a7e6f3
HamadKhushik <hammadkhan@live.com>
2022-02-05 23:49:17 +0000

    amazon shopping system ProductCategory

--- CTCI/src/objectOrientedDesign/amazonShoppingSystem/ProductCategory.java
+++ CTCI/src/objectOrientedDesign/amazonShoppingSystem/ProductCategory.java
@@ -1,11 +1,22 @@
-/**
- * 
- */
 package objectOrientedDesign.amazonShoppingSystem;
 
 /**
- * @author Hammad:  05-Feb-2022
+ * This file contains classes for ProductCategory, Product and ProductReview
+ * 
+ * @author Hammad: 05-Feb-2022
  */
 public class ProductCategory {
+	private String name;
+	private String description;
+}
+
+class ProductReview {
+	private int rating;
+	private String review;
+
+	private Member reviewer;
+}
+
+class Product {
 
 }
