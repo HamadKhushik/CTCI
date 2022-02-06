b04b28cb6724d508f6b5d2e22f40be80d003c770
HamadKhushik <hammadkhan@live.com>
2022-02-06 19:46:33 +0000

    amazon shopping system updated

--- CTCI/src/objectOrientedDesign/amazonShoppingSystem/OrderStatus.java
+++ CTCI/src/objectOrientedDesign/amazonShoppingSystem/OrderStatus.java
@@ -6,5 +6,46 @@ package objectOrientedDesign.amazonShoppingSystem;
  * @author Hammad: 05-Feb-2022
  */
 public enum OrderStatus {
+	UNSHIPPED,
+	PENDING,
+	SHIPPED,
+	COMPLETED,
+	CANCELLED,
+	REFUND_APPLIED
+}
 
+enum AccountStatus {
+	ACTIVE,
+	BLOCKED,
+	BANNED,
+	COMPROMISED,
+	ARCHIVED,
+	UNKNOWN
+}
+
+enum ShipmentStatus {
+	PENDING,
+	SHIPPED,
+	DELIVERED,
+	ON_HOLD
+}
+
+enum PaymentStatus {
+	UNPAID,
+	PENDING,
+	COMPLETED,
+	FILLED,
+	DECLINED,
+	CANCELLED,
+	BANDONED,
+	SETTLING,
+	SETTLED,
+	REFUNDED
+}
+
+class Address {
+	private String streetAddress;
+	private String city;
+	private String postcode;
+	private String country;
 }
